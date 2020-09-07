4f3672a4055a2275f0bebd633a722209d79a8785
Lucas <martinslf@taginfraestrutura.com.br>
2020-09-07 11:08:18 -0300

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { db } from "./models/database.js";
 import router from "./routes/transactionRouter.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send("Financial Control API"));
 app.use("/transactions", router);
 
 app.listen("4000", () => {
